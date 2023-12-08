e35d929ce2dbafdba1f64880b973cce24ba74d58
Maximilian Muecke <muecke.maximilian@gmail.com>
2023-12-08 23:23:07 +0100

    refactor: function renaming

--- R/wb.R
+++ R/wb.R
@@ -1,13 +1,13 @@
-fetch_worldbank <- function(country_code, indicator_code, page) {
+fetch_wb <- function(country, indicator, page) {
   request("http://api.worldbank.org/v2") |>
-    req_url_path_append("country", country_code, "indicator", indicator_code) |>
+    req_url_path_append("country", country, "indicator", indicator) |>
     req_url_query(format = "json", page = page) |>
     req_perform() |>
     resp_body_json()
 }
 
-worldbank_page <- function(indicator_code, country_code = "US", page = 1) {
-  resp <- fetch_worldbank(country_code, indicator_code, page)
+wb_page <- function(indicator, country = "US", page = 1) {
+  resp <- fetch_wb(country, indicator, page)
   npages <- resp[[1]]$pages
   data <- lapply(resp[[2]], \(row) {
     if (is.null(row$value) || is.null(row$date)) {
@@ -15,8 +15,8 @@ worldbank_page <- function(indicator_code, country_code = "US", page = 1) {
     }
     tibble(
       year = row$date,
-      country_code = country_code,
-      indicator_code = indicator_code,
+      country_code = country,
+      indicator_code = indicator,
       value = row$value
     )
   }) |>
@@ -25,14 +25,14 @@ worldbank_page <- function(indicator_code, country_code = "US", page = 1) {
   list(page = page, npages = npages, data = data)
 }
 
-worldbank_indicator <- function(indicator_code = "NY.GDP.MKTP.CD",
-                                country_code = "US") {
-  resp <- worldbank_page(indicator_code, country_code, 1)
+#' @export
+wb_indicator <- function(indicator = "NY.GDP.MKTP.CD", country = "US") {
+  resp <- wb_page(indicator, country, 1)
   if (resp$npages == 1) {
     return(resp$data)
   }
   lapply(seq(2, resp$npage), \(page) {
-    worldbank_page(indicator_code, country_code, page)$data
+    wb_page(indicator, country, page)$data
   }) |>
     bind_rows(resp$data) |>
     arrange(year)
