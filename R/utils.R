580a340a7fa7d5fe09a326e73b37ae236764b578
Maximilian Muecke <muecke.maximilian@gmail.com>
2023-12-10 13:58:43 +0100

    fix: add stats import for setNames

--- R/utils.R
+++ R/utils.R
@@ -24,7 +24,7 @@ map_mold <- function(.x, .f, .value, ...) {
   } else {
     vapply(.x, `[[`, .f, FUN.VALUE = .value, USE.NAMES = FALSE, ...)
   }
-  setNames(out, names(.x))
+  stats::setNames(out, names(.x))
 }
 
 map_chr <- function(.x, .f, ...) {
