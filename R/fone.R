ab7158dbde004d758b87d0a7de8934cd619a5a35
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-08-30 18:07:34 +0200

    chore: rename of file and fone functions

--- R/fone.R
+++ R/fone.R
@@ -1,19 +1,21 @@
+#' @source <https://financesone.worldbank.org/data>
 #' @examples
 #' \dontrun{
-#' wb_finances_data(dataset_id = "DS00005", resource_id = "RS00005")
+#' wb_fone_dataset(dataset_id = "DS00005", resource_id = "RS00005")
 #' }
-wb_finances_data <- function(dataset_id, resource_id, ...) {
+wb_fone_dataset <- function(dataset_id, resource_id, ...) {
   stopifnot(is_string(dataset_id), is_string(resource_id))
   wb_finances(
     resource = "apiservice", datasetId = dataset_id, resourceId = resource_id, ...
   )
 }
 
+#' @source <https://financesone.worldbank.org/data>
 #' @examples
 #' \dontrun{
-#' wb_finances_view("DS01538")
+#' wb_fone_view("DS01538")
 #' }
-wb_finances_view <- function(view_id, ...) {
+wb_fone_view <- function(view_id, ...) {
   stopifnot(is_string(view_id))
   wb_finances(resource = "view", viewId = view_id, ...)
 }
