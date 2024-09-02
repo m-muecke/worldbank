e499cd90dd5089145cb007bebb0a2c41a25fdd4b
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-09-02 21:37:17 +0200

    docs: more docs and renaming for fone

--- R/fone.R
+++ R/fone.R
@@ -1,26 +1,41 @@
+#' Return Finance One Dataset Data
+#'
+#' @param dataset_id `character(1)` id of the associated dataset.
+#' @param resoure_id `character(1)` id of the associated resource.
+#' @param ... Additional arguments passed to the request.
+#' @returns A `data.frame()` with the requested dataset.
 #' @source <https://financesone.worldbank.org/data>
+#' @family Finances One
+#' @export
 #' @examples
 #' \dontrun{
-#' wb_fone_dataset(dataset_id = "DS00005", resource_id = "RS00005")
+#' fone_dataset(dataset_id = "DS00005", resource_id = "RS00005")
 #' }
-wb_fone_dataset <- function(dataset_id, resource_id, ...) {
+fone_dataset <- function(dataset_id, resource_id, ...) {
   stopifnot(is_string(dataset_id), is_string(resource_id))
-  wb_finances(
+  fone(
     resource = "apiservice", datasetId = dataset_id, resourceId = resource_id, ...
   )
 }
 
+#' Return Finance One View Data
+#'
+#' @param view_id `character(1)` id of the view.
+#' @param ... Additional arguments passed to the request.
+#' @returns A `data.frame()` with the requested view.
 #' @source <https://financesone.worldbank.org/data>
+#' @family Finances One
+#' @export
 #' @examples
 #' \dontrun{
-#' wb_fone_view("DS01538")
+#' fone_view("DS01538")
 #' }
-wb_fone_view <- function(view_id, ...) {
+fone_view <- function(view_id, ...) {
   stopifnot(is_string(view_id))
-  wb_finances(resource = "view", viewId = view_id, ...)
+  fone(resource = "view", viewId = view_id, ...)
 }
 
-wb_finances <- function(resource, ...) {
+fone <- function(resource, ...) {
   resp <- request("https://datacatalogapi.worldbank.org/dexapps/fone/api") |>
     req_user_agent("worldbank (https://m-muecke.github.io/worldbank)") |>
     req_error(body = \(resp) resp_body_string(resp)) |>
