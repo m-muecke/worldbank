f37a4c403c21bd93549a966fff31e69672715f99
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-09-12 22:18:16 +0200

    tests: sanity checks for fone

---
+++ tests/testthat/test-fone.R
@@ -0,0 +1,27 @@
+test_that("fone_dataset basic checks", {
+  skip_if_offline()
+  skip_on_cran()
+  skip_on_ci()
+
+  res <- fone_dataset(dataset_id = "DS00047", resource_id = "RS00005")
+  expect_s3_class(res, "data.frame")
+  expect_gt(nrow(res), 100L)
+
+  res <- fone_dataset(dataset_id = "DS00047", resource_id = "RS00005", limit = 10L)
+  expect_s3_class(res, "data.frame")
+  expect_identical(dim(res), c(10L, 33L))
+})
+
+test_that("fone_view basic checks", {
+  skip_if_offline()
+  skip_on_cran()
+  skip_on_ci()
+
+  res <- fone_view(view_id = "DS01538")
+  expect_s3_class(res, "data.frame")
+  expect_gt(nrow(res), 100L)
+
+  res <- fone_view(view_id = "DS01538", limit = 10L)
+  expect_s3_class(res, "data.frame")
+  expect_identical(dim(res), c(10L, 6L))
+})
