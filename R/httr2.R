dc3d1392a3f3edff5109f2d2513acff75364502e
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-09-25 11:56:01 +0200

    refactor: read CSV bodies with `read.csv(text = )`

--- R/httr2.R
+++ R/httr2.R
@@ -18,8 +18,7 @@ wb_request <- function(url) {
 }
 
 resp_body_csv <- function(resp) {
-  body <- resp_body_string(resp, "UTF-8")
-  utils::read.csv(textConnection(body, encoding = "UTF-8"), na.strings = character())
+  utils::read.csv(text = resp_body_string(resp, "UTF-8"), na.strings = character())
 }
 
 resp_total_pages <- function(resp, per_page) {
